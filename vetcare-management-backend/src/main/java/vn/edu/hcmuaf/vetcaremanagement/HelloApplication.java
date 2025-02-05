d1456fb66055d2d4259a6c1202704b7180b4db1e
Tran Qui Bang <21130279@st.hcmuaf.edu.vn>
2025-02-05 12:29:03 +0700

    chore(backend): initial Jakarta EE setup

---
+++ vetcare-management-backend/src/main/java/vn/edu/hcmuaf/vetcaremanagement/HelloApplication.java
@@ -0,0 +1,9 @@
+package vn.edu.hcmuaf.vetcaremanagement;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/v1/api")
+public class HelloApplication extends Application {
+
+}
