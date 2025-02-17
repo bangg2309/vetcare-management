02d3911064b9a6ad0985b067534cc8d97465ec76
Tran Qui Bang <21130279@st.hcmuaf.edu.vn>
2025-02-17 15:20:49 +0700

    feat(back-end): config global exception

---
+++ vetcare-management-backend/src/main/java/vn/edu/hcmuaf/vetcaremanagement/exeption/ErrorCode.java
@@ -0,0 +1,24 @@
+package vn.edu.hcmuaf.vetcaremanagement.exeption;
+
+import jakarta.ws.rs.core.Response;
+import lombok.Getter;
+
+@Getter
+public enum ErrorCode {
+    UNCATEGORIZED_EXCEPTION(9999, "Uncategorized error", Response.Status.INTERNAL_SERVER_ERROR),
+    INVALID_KEY(1001, "Invalid data.", Response.Status.BAD_REQUEST),
+    UNAUTHORIZED(1002, "You do not have permission to access this resource.", Response.Status.FORBIDDEN),
+    RESOURCE_NOT_FOUND(1003, "Resource not found.", Response.Status.NOT_FOUND),
+    VALIDATION_FAILED(1004, "Input data is invalid.", Response.Status.BAD_REQUEST),
+    USER_NOT_FOUND(1005, "User not found.", Response.Status.NOT_FOUND);
+
+    ErrorCode(int code, String message, Response.Status statusCode) {
+        this.code = code;
+        this.message = message;
+        this.statusCode = statusCode;
+    }
+
+    private final int code;
+    private final String message;
+    private final Response.Status statusCode;
+}
